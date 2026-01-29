d10f9b29fb2ca5022132658b7b82cae3a7ade9c7
Corcorcoran2 <107974859+Corcorcoran2@users.noreply.github.com>
2026-01-28 19:33:46 -0800

    Starting documentation
    Adding documentation to some o the classes

--- module_human_leg.py
+++ module_human_leg.py
@@ -1,36 +1,38 @@
-import autorig.control_rig.module.base
+#Example implementation of the ModuleLeg class inheriting from ModuleBase. Actual node creation and
+#Maya specific logic is handled by feature classes through composition.
+
+import maya.cmds as cmds
+
 from autorig.control_rig.module.base import ModuleBase
-import autorig.control_rig.module.query as module_query
-import autorig.control_rig.module.utils as module_utils
 from autorig.control_rig.feature.FK import FeatureFK
 from autorig.control_rig.feature.IK import FeatureIK
-import autorig.control_rig.feature.foot_roll
 from autorig.control_rig.feature.foot_roll import FeatureFootRoll
+
+import autorig.control_rig.module.query as module_query
+import autorig.control_rig.module.utils as module_utils
 import autorig.control_rig.feature.switch as feature_switch
-import maya.cmds as cmds
-import importlib
-importlib.reload(autorig.control_rig.feature.foot_roll)
-importlib.reload(autorig.control_rig.module.base)
-importlib.reload(module_utils)
-importlib.reload(module_query)
-importlib.reload(autorig.control_rig.feature.IK)
 
 class ModuleHumanLeg(ModuleBase):
+    #Name is defined at class level to be added to MODULE_REGISTRY for UI detection
     cls_module_name = "human_leg"
     
     def __init__ (self, side, *args, **kwargs):
         super().__init__(side=side,*args, **kwargs)    
 
+    #Expected node name pattern to be suffixed for driver joints, FK, IK, etc.
     @property
     def ID_list(self):
         return [f"leg_{self.side}_1",f"leg_{self.side}_2",f"leg_{self.side}_3"]
     
+    #Compatible feature classes specified here along with ID list range to apply the system
     @property
     def supported_features(self):
         return {FeatureFK: self.ID_list,
                 FeatureIK: self.ID_list
                 }
     
+    #Features that require multiple modules built have additional checks to make sure they exist in scene.
+    #For example FeatureFootRoll can only be added to the rig if HumanLeg and HumanFoot are created first.
     @property
     def supported_multi_features(self):
         return {FeatureFootRoll: {
@@ -40,10 +42,12 @@ class ModuleHumanLeg(ModuleBase):
                 }
         }
     
+    #Module will attach to final joint of upstream module unless specified here.
     @property
     def attach_key(self):
         return {"human_spine_M": "spine_M_1"}
     
+    #ModuleLeg has additional checks in place to allow addition and removal of a switch system.
     def add_feature(self,feature):
         super().add_feature(feature)
         if feature == "FK" or feature == "IK":
@@ -61,18 +65,3 @@ class ModuleHumanLeg(ModuleBase):
         cmds.parent(loc, f"{self.instance_module_name}|guide")
 
         feature_switch.add_driver_switch(self.instance_module_name)
-
-if __name__ == "__main__":
-    import maya.cmds as cmds
-
-    current_scene = cmds.file(q=True, sn=True)
-
-    if current_scene:
-        cmds.file(current_scene, o=True, force=True)
-    else:
-        cmds.file(new=True, force=True)
-
-    instance = ModuleHumanLeg('L')
-    instance.create_module()
-    instance.add_feature("IK")
-    instance.add_feature("FK")
